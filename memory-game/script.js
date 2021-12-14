e15357053c23570743ecd6500f0a3082bfa8ec4b
dpaguba <88382171+dpaguba@users.noreply.github.com>
2021-12-14 12:54:14 +0100

    нужно еще прописать логику для addEventListener

--- memory-game/script.js
+++ memory-game/script.js
@@ -1,7 +1,14 @@
-const symbols = ["🐊", "🦖", "🐥", "🦞", "🐢", "🦀", "🦧", "🐆", "🐬",
-    "🐛", "🦋", "🐗", "🐐", "🦫", "🦜","🦔"];
+const symbols = ["🐊", "🦖", "🐥", "🦞", "🐢", "🦀", "🦧", "🐆", "🐬", "🐛", "🦋", "🐗", "🐐", "🦫", "🦜","🦔"];
 const bars = document.querySelectorAll(".bar");
 
+let barsWithSymbol = [];
+
+function initializeBars(){
+    for (let i = 0; i < bars.length; i++) {
+        barsWithSymbol.push("");
+    }
+}
+initializeBars();
 
 function generateNum(){
     return Math.floor(Math.random() * 32);
@@ -17,11 +24,52 @@ function fillBars(){
             i++;
         }else {
             let position = generateNum();
-            if (bars[position].innerText === "🐾"){
-                bars[position].innerHTML = symbols[i];
+            if (barsWithSymbol[position] === ""){
+                barsWithSymbol[position] = symbols[i];
                 count++;
             }
         }
     }
 }
-//fillBars();
+
+fillBars();
+
+function makeActive(i){
+    let active = document.querySelectorAll(".active");
+        if (active.length <= 1){
+            bars[i].innerHTML = barsWithSymbol[i];
+        }else if (active.length === 2){
+            if (active[0] === active[1]){
+                for (let j = 0; j < active.length; j++) {
+                    active[i].disabled = true;
+                }
+            }else {
+                bars[i].innerHTML = "🐾";
+            }
+        }
+}
+
+
+for (let i = 0; i < bars.length; i++) {
+
+    // bars[i].addEventListener("click", ()=>{
+    //     let active = document.querySelectorAll(".active");
+    //     if (+active <= 0){
+    //         bars[i].innerHTML = barsWithSymbol[i];
+    //     }else if (+active === 2){
+    //         if (active[0] === active[1]){
+    //             for (let j = 0; j < active.length; j++) {
+    //                 active[j].removeEventListener("click");
+    //             }
+    //         }else {
+    //
+    //         }
+    //     }else {
+    //
+    //     }
+    // });
+
+    bars[i].addEventListener("click", makeActive);
+}
+
+
