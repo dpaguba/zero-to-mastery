d8884f46dd0a6f2f8f11559ff1454abb77a322fc
Dmytro Pahuba <88382171+dpaguba@users.noreply.github.com>
2022-01-14 09:33:20 +0100

    describe func for btn ac

--- iCalculator/script.js
+++ iCalculator/script.js
@@ -1,1 +1,15 @@
+console.log("This calculator was made by dpaguba");
+let first = "";
+let second = "";
+let sign = "";
 
+const screen = document.getElementById("result");
+const numbers = document.querySelectorAll(".num");
+
+function clearAll(){
+    first = "";
+    second = "";
+    sign = "";
+    screen.innerText = "0";
+    changeSize();
+}
